7e1665c35efbf9afb2b933d1118e3fc2797f879a
nachat-ayoub <venomx2020x@gmail.com>
2023-04-19 13:30:01 +0000

    Change navbar button style

--- src/compenents/Navbar.js
+++ src/compenents/Navbar.js
@@ -1,14 +1,21 @@
 function Navbar() {
-    return(
-        <nav class="navbar bg-warning ">
-  <div class="container-fluid">
-    <a class="navbar-brand">Devhive</a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    )
+  return (
+    <nav class="navbar bg-warning ">
+      <div class="container-fluid">
+        <a class="navbar-brand">DevHive</a>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-dark" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
