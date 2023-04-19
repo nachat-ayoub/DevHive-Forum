f2a4dc6b057d99a45cbf759c8b7a24470aab7b66
whiteAsmaa <whiteasmaa831@gmail.com>
2023-04-19 14:04:16 +0000

    Add Navbar & Home

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../compenents/Navbar";
+
+function Home() {
+    return(
+        <div>
+            <Navbar />
+            <h1>Home</h1>
+        </div>
+
+      
+    )
+} 
+export default Home;
